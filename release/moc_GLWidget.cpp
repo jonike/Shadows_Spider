cae2344c7e0c832ef4f89961cd0c9606de730c25
bergjones <bergjones@gmail.com>
2015-03-29 11:35:06 -0400

    test commit
    testing immFix in new local loc

---
+++ release/moc_GLWidget.cpp
@@ -0,0 +1,131 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'GLWidget.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.4.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../src/GLWidget.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'GLWidget.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.4.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_GLWidget_t {
+    QByteArrayData data[5];
+    char stringdata[44];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_GLWidget_t, stringdata) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_GLWidget_t qt_meta_stringdata_GLWidget = {
+    {
+QT_MOC_LITERAL(0, 0, 8), // "GLWidget"
+QT_MOC_LITERAL(1, 9, 7), // "changed"
+QT_MOC_LITERAL(2, 17, 0), // ""
+QT_MOC_LITERAL(3, 18, 16), // "const QMimeData*"
+QT_MOC_LITERAL(4, 35, 8) // "mimeData"
+
+    },
+    "GLWidget\0changed\0\0const QMimeData*\0"
+    "mimeData"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_GLWidget[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       2,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       2,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    1,   24,    2, 0x06 /* Public */,
+       1,    0,   27,    2, 0x26 /* Public | MethodCloned */,
+
+ // signals: parameters
+    QMetaType::Void, 0x80000000 | 3,    4,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void GLWidget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        GLWidget *_t = static_cast<GLWidget *>(_o);
+        switch (_id) {
+        case 0: _t->changed((*reinterpret_cast< const QMimeData*(*)>(_a[1]))); break;
+        case 1: _t->changed(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        void **func = reinterpret_cast<void **>(_a[1]);
+        {
+            typedef void (GLWidget::*_t)(const QMimeData * );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&GLWidget::changed)) {
+                *result = 0;
+            }
+        }
+    }
+}
+
+const QMetaObject GLWidget::staticMetaObject = {
+    { &QGLWidget::staticMetaObject, qt_meta_stringdata_GLWidget.data,
+      qt_meta_data_GLWidget,  qt_static_metacall, 0, 0}
+};
+
+
+const QMetaObject *GLWidget::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *GLWidget::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_GLWidget.stringdata))
+        return static_cast<void*>(const_cast< GLWidget*>(this));
+    return QGLWidget::qt_metacast(_clname);
+}
+
+int GLWidget::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QGLWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 2)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 2;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void GLWidget::changed(const QMimeData * _t1)
+{
+    void *_a[] = { 0, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+QT_END_MOC_NAMESPACE
---
+++ release/moc_MainWin.cpp
@@ -0,0 +1,343 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'MainWin.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.4.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../src/MainWin.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'MainWin.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.4.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_MainWin_t {
+    QByteArrayData data[43];
+    char stringdata[449];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWin_t, stringdata) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWin_t qt_meta_stringdata_MainWin = {
+    {
+QT_MOC_LITERAL(0, 0, 7), // "MainWin"
+QT_MOC_LITERAL(1, 8, 16), // "progressAdd_init"
+QT_MOC_LITERAL(2, 25, 0), // ""
+QT_MOC_LITERAL(3, 26, 8), // "newScene"
+QT_MOC_LITERAL(4, 35, 9), // "openScene"
+QT_MOC_LITERAL(5, 45, 9), // "saveScene"
+QT_MOC_LITERAL(6, 55, 11), // "saveSceneAs"
+QT_MOC_LITERAL(7, 67, 11), // "quitConfirm"
+QT_MOC_LITERAL(8, 79, 12), // "startupScene"
+QT_MOC_LITERAL(9, 92, 9), // "pivCenter"
+QT_MOC_LITERAL(10, 102, 6), // "pivTgl"
+QT_MOC_LITERAL(11, 109, 7), // "delete_"
+QT_MOC_LITERAL(12, 117, 4), // "dupe"
+QT_MOC_LITERAL(13, 122, 4), // "hide"
+QT_MOC_LITERAL(14, 127, 6), // "unHide"
+QT_MOC_LITERAL(15, 134, 9), // "invertSel"
+QT_MOC_LITERAL(16, 144, 6), // "parent"
+QT_MOC_LITERAL(17, 151, 11), // "parentWorld"
+QT_MOC_LITERAL(18, 163, 7), // "hLayTgl"
+QT_MOC_LITERAL(19, 171, 7), // "vLayTgl"
+QT_MOC_LITERAL(20, 179, 10), // "gridLayTgl"
+QT_MOC_LITERAL(21, 190, 8), // "camLiAdd"
+QT_MOC_LITERAL(22, 199, 6), // "objAdd"
+QT_MOC_LITERAL(23, 206, 8), // "getChild"
+QT_MOC_LITERAL(24, 215, 9), // "getParent"
+QT_MOC_LITERAL(25, 225, 21), // "searchUp_intermediate"
+QT_MOC_LITERAL(26, 247, 18), // "shared_ptr<Object>"
+QT_MOC_LITERAL(27, 266, 8), // "searchUp"
+QT_MOC_LITERAL(28, 275, 7), // "pivHide"
+QT_MOC_LITERAL(29, 283, 14), // "gizSpaceSwitch"
+QT_MOC_LITERAL(30, 298, 7), // "gizShow"
+QT_MOC_LITERAL(31, 306, 6), // "string"
+QT_MOC_LITERAL(32, 313, 14), // "gizPivAutoShow"
+QT_MOC_LITERAL(33, 328, 11), // "PrefWinOpen"
+QT_MOC_LITERAL(34, 340, 10), // "TglCPopWin"
+QT_MOC_LITERAL(35, 351, 8), // "clearSel"
+QT_MOC_LITERAL(36, 360, 18), // "attrTableVizToggle"
+QT_MOC_LITERAL(37, 379, 12), // "pivMatchTest"
+QT_MOC_LITERAL(38, 392, 9), // "gammaCsel"
+QT_MOC_LITERAL(39, 402, 9), // "glm::vec3"
+QT_MOC_LITERAL(40, 412, 13), // "deferredPrefs"
+QT_MOC_LITERAL(41, 426, 8), // "gridInit"
+QT_MOC_LITERAL(42, 435, 13) // "GetTickCount2"
+
+    },
+    "MainWin\0progressAdd_init\0\0newScene\0"
+    "openScene\0saveScene\0saveSceneAs\0"
+    "quitConfirm\0startupScene\0pivCenter\0"
+    "pivTgl\0delete_\0dupe\0hide\0unHide\0"
+    "invertSel\0parent\0parentWorld\0hLayTgl\0"
+    "vLayTgl\0gridLayTgl\0camLiAdd\0objAdd\0"
+    "getChild\0getParent\0searchUp_intermediate\0"
+    "shared_ptr<Object>\0searchUp\0pivHide\0"
+    "gizSpaceSwitch\0gizShow\0string\0"
+    "gizPivAutoShow\0PrefWinOpen\0TglCPopWin\0"
+    "clearSel\0attrTableVizToggle\0pivMatchTest\0"
+    "gammaCsel\0glm::vec3\0deferredPrefs\0"
+    "gridInit\0GetTickCount2"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWin[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      38,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  204,    2, 0x0a /* Public */,
+       3,    0,  205,    2, 0x0a /* Public */,
+       4,    0,  206,    2, 0x0a /* Public */,
+       5,    0,  207,    2, 0x0a /* Public */,
+       6,    0,  208,    2, 0x0a /* Public */,
+       7,    0,  209,    2, 0x0a /* Public */,
+       8,    1,  210,    2, 0x0a /* Public */,
+       9,    0,  213,    2, 0x0a /* Public */,
+      10,    0,  214,    2, 0x0a /* Public */,
+      11,    0,  215,    2, 0x0a /* Public */,
+      12,    0,  216,    2, 0x0a /* Public */,
+      13,    0,  217,    2, 0x0a /* Public */,
+      14,    0,  218,    2, 0x0a /* Public */,
+      15,    0,  219,    2, 0x0a /* Public */,
+      16,    0,  220,    2, 0x0a /* Public */,
+      17,    0,  221,    2, 0x0a /* Public */,
+      18,    0,  222,    2, 0x0a /* Public */,
+      19,    0,  223,    2, 0x0a /* Public */,
+      20,    0,  224,    2, 0x0a /* Public */,
+      21,    0,  225,    2, 0x0a /* Public */,
+      22,    2,  226,    2, 0x0a /* Public */,
+      23,    0,  231,    2, 0x0a /* Public */,
+      24,    0,  232,    2, 0x0a /* Public */,
+      25,    1,  233,    2, 0x0a /* Public */,
+      27,    1,  236,    2, 0x0a /* Public */,
+      28,    0,  239,    2, 0x0a /* Public */,
+      29,    0,  240,    2, 0x0a /* Public */,
+      30,    1,  241,    2, 0x0a /* Public */,
+      32,    0,  244,    2, 0x0a /* Public */,
+      33,    0,  245,    2, 0x0a /* Public */,
+      34,    0,  246,    2, 0x0a /* Public */,
+      35,    0,  247,    2, 0x0a /* Public */,
+      36,    0,  248,    2, 0x0a /* Public */,
+      37,    0,  249,    2, 0x0a /* Public */,
+      38,    0,  250,    2, 0x0a /* Public */,
+      40,    0,  251,    2, 0x0a /* Public */,
+      41,    0,  252,    2, 0x0a /* Public */,
+      42,    0,  253,    2, 0x0a /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    2,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString, QMetaType::QString,    2,    2,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 26,    2,
+    QMetaType::Bool, 0x80000000 | 26,    2,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 31,    2,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    0x80000000 | 39,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Double,
+
+       0        // eod
+};
+
+void MainWin::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWin *_t = static_cast<MainWin *>(_o);
+        switch (_id) {
+        case 0: _t->progressAdd_init(); break;
+        case 1: _t->newScene(); break;
+        case 2: _t->openScene(); break;
+        case 3: _t->saveScene(); break;
+        case 4: _t->saveSceneAs(); break;
+        case 5: _t->quitConfirm(); break;
+        case 6: _t->startupScene((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 7: _t->pivCenter(); break;
+        case 8: _t->pivTgl(); break;
+        case 9: _t->delete_(); break;
+        case 10: _t->dupe(); break;
+        case 11: _t->hide(); break;
+        case 12: _t->unHide(); break;
+        case 13: _t->invertSel(); break;
+        case 14: _t->parent(); break;
+        case 15: _t->parentWorld(); break;
+        case 16: _t->hLayTgl(); break;
+        case 17: _t->vLayTgl(); break;
+        case 18: _t->gridLayTgl(); break;
+        case 19: _t->camLiAdd(); break;
+        case 20: _t->objAdd((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< QString(*)>(_a[2]))); break;
+        case 21: _t->getChild(); break;
+        case 22: _t->getParent(); break;
+        case 23: _t->searchUp_intermediate((*reinterpret_cast< shared_ptr<Object>(*)>(_a[1]))); break;
+        case 24: { bool _r = _t->searchUp((*reinterpret_cast< shared_ptr<Object>(*)>(_a[1])));
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = _r; }  break;
+        case 25: _t->pivHide(); break;
+        case 26: _t->gizSpaceSwitch(); break;
+        case 27: _t->gizShow((*reinterpret_cast< string(*)>(_a[1]))); break;
+        case 28: _t->gizPivAutoShow(); break;
+        case 29: _t->PrefWinOpen(); break;
+        case 30: _t->TglCPopWin(); break;
+        case 31: _t->clearSel(); break;
+        case 32: _t->attrTableVizToggle(); break;
+        case 33: _t->pivMatchTest(); break;
+        case 34: { glm::vec3 _r = _t->gammaCsel();
+            if (_a[0]) *reinterpret_cast< glm::vec3*>(_a[0]) = _r; }  break;
+        case 35: _t->deferredPrefs(); break;
+        case 36: _t->gridInit(); break;
+        case 37: { double _r = _t->GetTickCount2();
+            if (_a[0]) *reinterpret_cast< double*>(_a[0]) = _r; }  break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject MainWin::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWin.data,
+      qt_meta_data_MainWin,  qt_static_metacall, 0, 0}
+};
+
+
+const QMetaObject *MainWin::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWin::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWin.stringdata))
+        return static_cast<void*>(const_cast< MainWin*>(this));
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWin::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 38)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 38;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 38)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 38;
+    }
+    return _id;
+}
+struct qt_meta_stringdata_Combo_t {
+    QByteArrayData data[1];
+    char stringdata[6];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_Combo_t, stringdata) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_Combo_t qt_meta_stringdata_Combo = {
+    {
+QT_MOC_LITERAL(0, 0, 5) // "Combo"
+
+    },
+    "Combo"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Combo[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       0,    0, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+       0        // eod
+};
+
+void Combo::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    Q_UNUSED(_o);
+    Q_UNUSED(_id);
+    Q_UNUSED(_c);
+    Q_UNUSED(_a);
+}
+
+const QMetaObject Combo::staticMetaObject = {
+    { &QComboBox::staticMetaObject, qt_meta_stringdata_Combo.data,
+      qt_meta_data_Combo,  qt_static_metacall, 0, 0}
+};
+
+
+const QMetaObject *Combo::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Combo::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_Combo.stringdata))
+        return static_cast<void*>(const_cast< Combo*>(this));
+    return QComboBox::qt_metacast(_clname);
+}
+
+int Combo::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QComboBox::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    return _id;
+}
+QT_END_MOC_NAMESPACE
